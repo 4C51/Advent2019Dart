388a95a8d1867e3f9aca580ed2f573e6ed6f2261
Ella Quinn Lagerquist <ellaquinn.lagerquist@amitracks.com>
2019-12-03 13:06:14 -0800

    Day 3 changed to just use vectors for intersection calcs

--- lib/advent_common/lib/src/rocketry/fuel_management.dart
+++ lib/advent_common/lib/src/rocketry/fuel_management.dart
@@ -1,4 +1,5 @@
 class FuelCircuit {
+  final _vectors = Set<FuelVector>();
   final _points = Set<FuelPoint>();
   final origin = FuelPoint(0, 0, null);
   Set<FuelPoint> _intersections = Set();
@@ -15,25 +16,28 @@ class FuelCircuit {
 
     for (var i = 0; i < vectors.length; i++) {
       var vector = FuelVector.fromString(vectors[i], previousOrigin, wire);
-      var newPoints = vector.getPoints();
-      _intersections.addAll(newPoints
-          .where((point) => _points.any((other) => point.intersects(other))));
-      _points.addAll(newPoints);
+      _vectors.add(vector);
       previousOrigin = vector.endPoint;
     }
   }
 
-  addVector(FuelVector vector) {
-    _points.addAll(vector.getPoints());
-  }
-
   List<FuelPoint> getIntersections() {
-    return _intersections.toList();
+    var ints = List<FuelPoint>();
+    var intersections = _vectors.fold<List<FuelPoint>>(ints, (ints, vector) {
+      ints.addAll(vector.intersections(_vectors));
+      return ints;
+    }).toSet().toList();
+    intersections.removeWhere((intersection) => intersection == null);
+    // intersections.fold<Set<FuelPoint>>(ints, (ints, intSet) {
+    //   ints.addAll(intSet.toList());
+    //   return ints;
+    // });
+    return intersections;
   }
 
   FuelPoint getClosestIntersection() {
     var intersections = getIntersections();
-    return intersections.where((inter) => inter.wire == 1).reduce(
+    return intersections.reduce(
         (interA, interB) =>
             origin.distanceTo(interA) < origin.distanceTo(interB)
                 ? interA
@@ -91,7 +95,12 @@ class FuelPoint {
   FuelPoint(this.x, this.y, this.wire);
 
   bool operator ==(dynamic other) =>
-      other is FuelPoint && x == other.x && y == other.y && wire == other.wire;
+      other is FuelPoint && x == other.x && y == other.y;
+
+  @override
+  int get hashCode {
+    return x * 1000000 + y;
+  }
 
   distanceTo(FuelPoint other) {
     var dx = x - other.x;
@@ -110,27 +119,55 @@ class FuelVector {
   final FuelPoint origin;
   final String direction;
   final int magnitude;
+  int wire;
   FuelPoint endPoint;
 
   FuelVector(this.origin, this.direction, this.magnitude, {int wire}) {
-    var dFactor = (direction == 'D' || direction == 'L') ? -1 : 1;
-    var endX, endY;
-
-    if (direction == 'U' || direction == 'D') {
-      endY = origin.y + magnitude * dFactor;
-    } else {
-      endX = origin.x + magnitude * dFactor;
-    }
-
-    endPoint =
-        FuelPoint(endX ?? origin.x, endY ?? origin.y, wire ?? origin.wire);
+    var scalar = _getScalar();
+    wire = wire ?? origin.wire;
+    endPoint = FuelPoint(origin.x + 1 * scalar[0], origin.y + 1 * scalar[1], wire ?? origin.wire);
   }
+
+  bool operator ==(dynamic other) =>
+      other is FuelVector && origin == other.origin && magnitude == other.magnitude && wire == other.wire;
 
   static FuelVector fromString(String vectorString, FuelPoint origin,
       [int wire]) {
     return FuelVector(
         origin, vectorString[0], int.parse(vectorString.substring(1)),
         wire: wire);
+  }
+
+  FuelPoint intersects(FuelVector other) {
+    var p1 = origin;
+    var p2 = endPoint;
+    var p3 = other.origin;
+    var p4 = other.endPoint;
+
+    if (p1 == p4 || p2 == p3) return null;
+
+    var det = ((p4.x - p3.x)*(p1.y - p2.y) - (p1.x - p2.x)*(p4.y - p3.y));
+
+    var t = ((p3.y - p4.y)*(p1.x - p3.x) + (p4.x - p3.x)*(p1.y - p3.y))/det;
+    if (t.isNaN || 0 > t || t > 1) return null;
+
+    var u = ((p1.y - p2.y)*(p1.x - p3.x) + (p2.x - p1.x)*(p1.y - p3.y))/det;
+    if (u.isNaN || 0 > u || u > 1) return null;
+    // print('t: $t');
+    // print('u: $u');
+
+    if (0 <= t && t <= 1 && 0 <= u && u <= 1) {
+      var ix = (p1.x + (t * (p2.x - p1.x))).toInt();
+      var iy = (p1.y + (t * (p2.y - p1.y))).toInt();
+      if (ix == 0 && iy == 0) return null;
+      return FuelPoint(ix, iy, null);
+    }
+
+    return null;
+  }
+
+  List<FuelPoint> intersections(Set<FuelVector> vectors) {
+    return vectors.map<FuelPoint>((otherVector) => intersects(otherVector)).toList();
   }
 
   Set<FuelPoint> getPoints() {
@@ -150,5 +187,20 @@ class FuelVector {
     }
 
     return points;
+  }
+
+  _getScalar() {
+    switch (direction) {
+      case 'U':
+        return [0, magnitude];
+      case 'D':
+        return [0, -1 * magnitude];
+      case 'L':
+        return [-1 * magnitude, 0];
+      case 'R':
+        return [magnitude, 0];
+      default:
+        return [0, 0];
+    }
   }
 }
