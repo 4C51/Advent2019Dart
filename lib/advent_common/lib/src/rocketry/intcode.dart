8e985b00e9eb88a87ee8bdaeebde297889c27f55
Ella Quinn Lagerquist <ellaquinn.lagerquist@amitracks.com>
2019-12-04 22:56:11 -0800

    Refactored Intcode computer to support Day 5

--- lib/advent_common/lib/src/rocketry/intcode.dart
+++ lib/advent_common/lib/src/rocketry/intcode.dart
@@ -2,22 +2,25 @@ class Intcode {
   String _program;
   List<int> _memory;
   int _pointer;
+  int _input;
+  int _output;
 
-  Intcode loadProgram(String input) {
-    _program = input;
-    var programInputs = input.split(',').map((i) => int.parse(i)).toList();
+  Intcode loadProgram(String programInput) {
+    _program = programInput;
+    var programInputs =
+        programInput.split(',').map((i) => int.parse(i)).toList();
     _memory = programInputs;
     return this;
   }
 
-  Intcode execute() {
+  Intcode execute([int input]) {
     if (_memory == null) throw 'Load program first.';
     var haltAndCatchFire = false;
     _pointer = 0;
+    _input = input;
 
     while (!haltAndCatchFire) {
-      haltAndCatchFire =
-          _opcodeExecute(_memory[_pointer]);
+      haltAndCatchFire = _opcodeExecute(_memory[_pointer]);
     }
 
     return this;
@@ -30,47 +33,103 @@ class Intcode {
 
   int readMem(int address) => _memory[address];
 
-  get output => _memory[0];
+  get output => _output != null ? _output : _memory[0];
 
   readMemAll() => _memory.join(',');
 
   reset() => loadProgram(_program);
 
-  _opcodeExecute(int opcode) {
+  _opcodeExecute(int insCode) {
     var opcodeMap = {
       1: _opcodeAdd,
       2: _opcodeMultiply,
-      99: () => true
+      3: _opcodeInput,
+      4: _opcodeOutput,
+      99: (_) => true
     };
 
-    return opcodeMap[opcode]?.call() ?? (throw 'Invalid opcode: ${opcode}');
+    var ins = Instruction(insCode, _pointer, _memory);
+
+    return opcodeMap[ins.opcode]?.call(ins) ??
+        (throw 'Invalid opcode: ${ins.opcode}');
   }
 
-  Instructions _getInstructions(int outputOffset, int inputCount) {
-    var ins = Instructions();
-    ins.outputAddress = _memory[_pointer + outputOffset];
-    for (var i = 1; i <= inputCount; i++) {
-      ins.inputs.add(_memory[_memory[_pointer + i]]);
+  List<int> _getParams(int inputCount) {
+    return _memory.getRange(_pointer + 1, _pointer + 1 + inputCount).toList();
+  }
+
+  _opcodeAdd(Instruction ins) {
+    ins.setParams(_getParams(3), 2);
+    _memory[ins.outputAddress] = ins.params[0] + ins.params[1];
+    _pointer += 4;
+    return false;
+  }
+
+  _opcodeMultiply(Instruction ins) {
+    ins.setParams(_getParams(3), 2);
+    _memory[ins.outputAddress] = ins.params[0] * ins.params[1];
+    _pointer += 4;
+    return false;
+  }
+
+  _opcodeInput(Instruction ins) {
+    ins.setParams(_getParams(1), 0);
+    _memory[ins.outputAddress] = _input;
+    _pointer += 2;
+    return false;
+  }
+
+  _opcodeOutput(Instruction ins) {
+    ins.setParams(_getParams(1));
+    _output = ins.params[0];
+    _pointer += 2;
+    return false;
+  }
+}
+
+class Instruction {
+  final int insCode;
+  final List<int> _memory;
+  final int address;
+  List<int> _params;
+  List<Mode> _modes;
+  int _outputAddress;
+  int opcode;
+
+  Instruction(this.insCode, this.address, this._memory) {
+    opcode = insCode % 100;
+  }
+
+  setParams(List<int> params, [int outputParam]) {
+    _params = params;
+    _modes = (insCode ~/ 100)
+        .toString()
+        .padLeft(3, '0')
+        .split('')
+        .reversed
+        .toList()
+        .map<Mode>((m) => Mode.values[int.parse(m)])
+        .toList();
+
+    for (var i = 0; i < params.length; i++) {
+      _params[i] = _getParamValue(_modes[i], params[i], outputParam == i);
     }
-    return ins;
   }
 
-  _opcodeAdd() {
-    var ins = _getInstructions(3, 2);
-    _memory[ins.outputAddress] = ins.inputs[0] + ins.inputs[1];
-    _pointer += 4;
-    return false;
-  }
+  get params => _params;
 
-  _opcodeMultiply() {
-    var ins = _getInstructions(3, 2);
-    _memory[ins.outputAddress] = ins.inputs[0] * ins.inputs[1];
-    _pointer += 4;
-    return false;
+  get outputAddress => _outputAddress;
+
+  int _getParamValue(Mode mode, int value, [bool isOutput = false]) {
+    if (isOutput) {
+      _outputAddress = value;
+      return value;
+    }
+
+    if (mode == Mode.Position) return _memory[value];
+    if (mode == Mode.Immediate) return value;
+    throw 'Unknown mode';
   }
 }
 
-class Instructions {
-  int outputAddress;
-  List<int> inputs = [];
-}
+enum Mode { Position, Immediate }
